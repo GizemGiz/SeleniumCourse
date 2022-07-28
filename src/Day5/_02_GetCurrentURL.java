bfa1d6002ce58fdaf615fe1592d48b6d396c512f
gizem <gizemmcevirgen@gmail.com>
2022-07-28 16:23:44 -0500

    Selenium WebDriver Methods - getCurrentURL method

---
+++ src/Day5/_02_GetCurrentURL.java
@@ -0,0 +1,23 @@
+package Day5;
+
+import org.checkerframework.checker.units.qual.Current;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class _02_GetCurrentURL {
+
+    public static void main(String[] args) {
+
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\gizem\\Selenium Tool\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://techno.study/");
+
+        String currentURL = driver.getCurrentUrl();
+        System.out.println("Current URL: " + currentURL);
+
+        driver.quit();
+    }
+}
