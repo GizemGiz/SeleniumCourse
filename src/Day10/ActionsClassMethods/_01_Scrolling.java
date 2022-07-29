7bd18f2d35e75ebcc6f6546e2e749849077dbf33
gizem <gizemmcevirgen@gmail.com>
2022-07-28 20:28:39 -0500

    Scrolling - JavascriptExecutor

---
+++ src/Day10/ActionsClassMethods/_01_Scrolling.java
@@ -0,0 +1,28 @@
+package Day10.ActionsClassMethods;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class _01_Scrolling {
+    public static void main(String[] args) throws InterruptedException {
+
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\gizem\\Selenium Tool\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://techno.study/");
+
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        // scroll down 3000 pixels
+        js.executeScript("window.scrollBy(0, 3000)");
+
+
+        // scroll up 2000 pixels
+        js.executeScript("window.scrollBy(0,-2000)");
+
+    }
+}
